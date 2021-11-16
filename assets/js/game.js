9a091c15f1ef7dd7ba4bc41117f6266938afe2e2
Juan Chavez <juan@Juans-Air-12.home>
2021-11-16 04:22:19 -0800

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -60,10 +60,15 @@ var fight=function(enemyName){
     }
 };
 
-//fight();
-
 for(var i = 0; i < enemyNames.length; i++) {
-   var pickedEnemyName=enemyNames[i];
+    if(playerHealth>0){
+        window.alert("Welcome to Robot Gladiators! Round " + (i+1));
+    }
+    else{
+        window.alert("You have lost your robot in battle. Gamer Over!");
+        break;
+    }
+    var pickedEnemyName=enemyNames[i];
     enemyHealth=50;
     fight(pickedEnemyName);
   }
