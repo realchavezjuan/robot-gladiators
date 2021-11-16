3f1a45193e643ef4a5cf7952be8b8292549ebb4e
Juan Chavez <juan@Juans-Air-12.home>
2021-11-16 01:32:32 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,27 @@
-window.alert("Welcome to Robot Gladiators!");
+window.alert("Lets Play!");
 var playerName=window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney=10;
-console.log(playerName, playerHealth, playerAttack);
+console.log(playerName, playerHealth, playerAttack, playerMoney);
 
-var enemyName="Roborto";
+var enemyNames=["Roborto","Amy Android","Robo Trumble"];
 var enemyHealth="50";
 var enemyAttack="12";
 
-var fight=function(){    
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
+//fight function
+var fight=function(enemyName){
+    window.alert("Welcome to Robot Gladiators!");
+    //Ask user if they want to fight
     var promptFight=window.prompt(
         "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
     );
-    
+    //user chooses to fight
     if(promptFight === "fight" || promptFight === "FIGHT"){
         enemyHealth=enemyHealth-playerAttack;
         console.log(
@@ -24,20 +32,21 @@ var fight=function(){
         console.log(
             enemyName+" attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining."
         )
-
+// check enemy's health
         if(enemyHealth<=0){
             window.alert(enemyName+" has died!");
         } 
         else{
             window.alert(enemyName+" still has "+enemyHealth+" health left.");
         }
-
+//check player's health
         if(playerHealth<=0){
             window.alert(playerName+" has died!");
         }
         else{
             window.alert(playerName+" still has "+playerHealth+" health left.");
         }
+    //user chooses to skip fight
     }else if (promptFight==="skip"||promptFight==="SKIP"){
         window.alert(playerName+" has choosen to skip the fight.");
         var confirmSkip=window.confirm("Are you sure you'd like to quit?");
@@ -53,4 +62,8 @@ var fight=function(){
     }
 };
 
-fight();
+//fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
