f9c89d77100faf191f29535a8763395c354c7b1c
Juan Chavez <juan@Juans-Air-12.home>
2021-11-15 22:41:39 -0800

    added conditions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,37 @@
 var playerName=window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+console.log(playerName, playerHealth, playerAttack);
 
-console.log(playerName);
+var enemyName="Roborto";
+var enemyHealth="50";
+var enemyAttack="12";
 
-function fight(){
-    window.alert("The fight has begun");
-}
+var fight=function(){
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth=enemyHealth-playerAttack;
+    console.log(
+    playerName + " attacked " + enemyName + ". "+enemyName+" now has "+enemyHealth+" health remaining."
+    );
+
+    playerHealth=playerHealth-enemyAttack;
+    console.log(
+        enemyName+" attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining."
+    )
+
+    if(enemyHealth<=0){
+        window.alert(enemyName+" has died!");
+    } 
+    else{
+        window.alert(enemyName+" still has "+enemyHealth+" health left.");
+    }
+
+    if(playerHealth<=0){
+        window.alert(playerName+" has died!");
+    }
+    else{
+        window.alert(playerName+" still has "+playerHealth+" health left.");
+    }
+};
 
 fight();
