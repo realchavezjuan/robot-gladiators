ea17f415626edc55d927e229c28b98e935114afc
Juan Chavez <juan@Juans-Air-12.home>
2021-11-19 22:51:01 -0800

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,19 +27,21 @@ var fight=function(enemyName){
             var confirmSkip=window.confirm("Are you sure you'd like to quit?");
             if(confirmSkip){
                 window.alert(playerName+" has decided to skip this fight. Goodbye.");
-                playerMoney=playerMoney-10;
+                playerMoney=Max.math(0,playerMoney-10);
                 console.log("playerMoney: ",playerMoney)
                 break;
             }
         }
     
         //user chooses to fight
-        enemyHealth=enemyHealth-playerAttack;
+        var damage=randomNumber(playerAttack-3,playerAttack);
+        enemyHealth=Math.max(0,enemyHealth-damage);
         console.log(
         playerName + " attacked " + enemyName + ". "+enemyName+" now has "+enemyHealth+" health remaining."
         );
-    
-        playerHealth=playerHealth-enemyAttack;
+        
+        var damage=randomNumber(enemyAttack-3,enemyAttack);
+        playerHealth=Math.max(0,playerHealth-damage);
         console.log(
             enemyName+" attacked "+playerName+". "+playerName+" now has "+playerHealth+" health remaining."
         )
@@ -60,6 +62,7 @@ var fight=function(enemyName){
     }
 };
 
+//start game function
 var startGame=function(){
 
     //reset player stats
@@ -77,7 +80,7 @@ var startGame=function(){
             break;
         }
         var pickedEnemyName=enemyNames[i];
-        enemyHealth=50;
+        enemyHealth=randomNumber(40,60);
         fight(pickedEnemyName);
 
         //if we're not at the last enemy in the array
@@ -167,5 +170,12 @@ var shop = function(){
         break;
     }
 }
+
+//function to generate a random numeric value
+var randomNumber=function(min,max){
+    var value=Math.floor(Math.random()*(max-min+1)+min);
+    return value;
+}
+
 //start game
 startGame();
